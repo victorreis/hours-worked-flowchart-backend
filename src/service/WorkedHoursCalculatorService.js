cc55c286b0bbf5e80f59b82fa7ece25d5461b631
victor.reis <VCREIS@brasilseg.com.br>
2020-09-01 03:35:11 -0300

    Calculations done

--- src/service/WorkedHoursCalculatorService.js
+++ src/service/WorkedHoursCalculatorService.js
@@ -10,7 +10,7 @@ const createMoments = (startTime, finishTime) => {
     const finishMoment = createMomentFromString(finishTime);
 
     if (startMoment.isAfter(finishMoment)) {
-        finishMoment.add({day: 1});
+        startMoment.subtract({days: 1});
     }
 
     return {
@@ -30,7 +30,7 @@ const createMomentFromHourMinute = (hour, minute) => {
         Number.isInteger(Number.parseInt(hour)) &&
         Number.isInteger(Number.parseInt(minute))
     ) {
-        return moment().set({hour, minute, second: 0});
+        return moment().set({hour, minute, second: 0, millisecond: 0});
     }
     throw 'Number conversion error.';
 };
@@ -52,42 +52,125 @@ const randomMoment = () => {
     });
 };
 
-const zeroFilled = (number) => ('00' + Math.round(number)).slice(-2);
+const zeroFilled = (number) => ('00' + Math.ceil(number)).slice(-2);
 
 const calculateDifference = (moment1, moment2) => {
+    const differenceInDays = moment1.diff(moment2, 'days');
     const differenceInHours = moment1.diff(moment2, 'hours');
     const differenceInMinutes = moment1.diff(moment2, 'minutes');
-    const hours = zeroFilled(differenceInHours);
-    const minutes = zeroFilled(differenceInMinutes - differenceInHours * 60);
+    const hours = zeroFilled(
+        differenceInHours - Math.ceil(differenceInDays * 24)
+    );
+    const minutes = zeroFilled(
+        differenceInMinutes - Math.ceil(differenceInHours * 60)
+    );
+
+    return `${hours}:${minutes}`;
+};
+
+const sumCalculatedDifferences = (d1, d2, d3) => {
+    const [d1Hour, d1Minute] = d1.split(':');
+    const [d2Hour, d2Minute] = d2.split(':');
+    const [d3Hour, d3Minute] = d3 ? d2.split(':') : [0, 0];
+    const hours = zeroFilled(
+        Number.parseInt(d1Hour) +
+            Number.parseInt(d2Hour) +
+            Number.parseInt(d3Hour)
+    );
+    const minutes = zeroFilled(
+        Number.parseInt(d1Minute) +
+            Number.parseInt(d2Minute) +
+            Number.parseInt(d3Minute)
+    );
+
+    if (Number.parseInt(minutes) >= 60) {
+        const newHours = zeroFilled(Number.parseInt(hours) + 1);
+        const newMinutes = zeroFilled(Number.parseInt(minutes) - 60);
+        return `${newHours}:${newMinutes}`;
+    }
+
     return `${hours}:${minutes}`;
 };
 
 const ZERO_HOUR = '00:00';
+const ALL_DAY = '17:00';
+const ALL_NIGHT = '07:00';
+const FIRST_HOURS_NIGHT = '05:00';
+const LAST_HOURS_NIGHT = '02:00';
+
 const zeroHourMoment = createMomentFromHourMinute(0, 0);
+const zeroHourMomentMinus1Day = createMomentFromHourMinute(0, 0).subtract(
+    1,
+    'days'
+);
+
+const nightMomentLimit = createMomentFromHourMinute(5, 00);
+const nightMomentLimitMinus1Day = createMomentFromHourMinute(5, 00).subtract(
+    1,
+    'days'
+);
+
+const daytimeMomentLimit = createMomentFromHourMinute(22, 00);
+const daytimeMomentLimitMinus1Day = createMomentFromHourMinute(22, 00).subtract(
+    1,
+    'days'
+);
+
+const twentyFourHoursMoment = createMomentFromHourMinute(24, 00);
+const twentyFourHoursMomentMinus1Day = createMomentFromHourMinute(
+    24,
+    00
+).subtract(1, 'days');
+
 const calculate = {
-    s1f1: (m1, m2) => {
-        return {
-            daytimeHours: ZERO_HOUR,
-            nightHours: calculateDifference(m2, m1),
-        };
-    },
-    s1f2: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    s1f3: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    s2f1: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    s2f2: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    s2f3: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    s3f1: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    s3f2: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    s3f3: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    f1s1: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    f1s2: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    f1s3: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    f2s1: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    f2s2: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    f2s3: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    f3s1: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    f3s2: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
-    f3s3: (m1, m2) => ({daytimeHours: m1, nightHours: m2}),
+    s1f1: (s1, f1) => ({
+        daytimeHours: ZERO_HOUR,
+        nightHours: calculateDifference(f1, s1),
+    }),
+    s1f2: (s1, f2) => ({
+        daytimeHours: calculateDifference(f2, nightMomentLimit),
+        nightHours: calculateDifference(nightMomentLimit, s1),
+    }),
+    s1f3: (s1, f3) => ({
+        daytimeHours: ALL_DAY,
+        nightHours: sumCalculatedDifferences(
+            calculateDifference(nightMomentLimit, s1),
+            calculateDifference(f3, daytimeMomentLimit)
+        ),
+    }),
+    s2f1: (s2, f1) => ({
+        daytimeHours: calculateDifference(daytimeMomentLimit, s2),
+        nightHours: sumCalculatedDifferences(
+            calculateDifference(twentyFourHoursMoment, daytimeMomentLimit),
+            calculateDifference(f1, zeroHourMoment)
+        ),
+    }),
+    s2f2: (s2, f2) => ({
+        daytimeHours: calculateDifference(f2, s2),
+        nightHours: ZERO_HOUR,
+    }),
+    s2f3: (s2, f3) => ({
+        daytimeHours: calculateDifference(daytimeMomentLimit, s2),
+        nightHours: calculateDifference(f3, daytimeMomentLimit),
+    }),
+    s3f1: (s3, f1) => ({
+        daytimeHours: ZERO_HOUR,
+        nightHours: sumCalculatedDifferences(
+            calculateDifference(twentyFourHoursMoment, s3),
+            calculateDifference(f1, zeroHourMoment)
+        ),
+    }),
+    s3f2: (s3, f2) => ({
+        daytimeHours: calculateDifference(f2, nightMomentLimit),
+        nightHours: sumCalculatedDifferences(
+            calculateDifference(twentyFourHoursMoment, s3),
+            calculateDifference(nightMomentLimit, zeroHourMoment)
+        ),
+    }),
+    s3f3: (s3, f3) => ({
+        daytimeHours: ZERO_HOUR,
+        nightHours: calculateDifference(f3, s3),
+    }),
 };
 
 const determineLetters = (startMoment, finishMoment) => {
@@ -97,19 +180,20 @@ const determineLetters = (startMoment, finishMoment) => {
     return {letter1: 'f', letter2: 's'};
 };
 
-// Eu poderia ter usado algum design patter nessa função para deixar
-// ela com menos ifs, mas estou com pouco tempo.
-const determineNumbers = (startMoment, finishMoment) => {
-    const nightMomentLimit = createMomentFromHourMinute(4, 59);
-    const daytimeMomentLimit = createMomentFromHourMinute(21, 59);
-    const twentyFourHoursMoment = createMomentFromHourMinute(23, 59);
-
-    const isStartBetween0And1stLimit = startMoment.isBetween(
-        zeroHourMoment,
-        nightMomentLimit,
-        undefined,
-        '[]'
-    );
+const isBetweenHours = (startMoment, finishMoment) => {
+    const isStartBetween0And1stLimit =
+        startMoment.isBetween(
+            zeroHourMoment,
+            nightMomentLimit,
+            undefined,
+            '[]'
+        ) ||
+        startMoment.isBetween(
+            zeroHourMomentMinus1Day,
+            nightMomentLimitMinus1Day,
+            undefined,
+            '[]'
+        );
     const isFinishBetween0And1stLimit = finishMoment.isBetween(
         zeroHourMoment,
         nightMomentLimit,
@@ -117,12 +201,19 @@ const determineNumbers = (startMoment, finishMoment) => {
         '[]'
     );
 
-    const isStartBetween1stLimitAnd2ndLimit = startMoment.isBetween(
-        nightMomentLimit,
-        daytimeMomentLimit,
-        undefined,
-        '[]'
-    );
+    const isStartBetween1stLimitAnd2ndLimit =
+        startMoment.isBetween(
+            nightMomentLimit,
+            daytimeMomentLimit,
+            undefined,
+            '[]'
+        ) ||
+        startMoment.isBetween(
+            nightMomentLimitMinus1Day,
+            daytimeMomentLimitMinus1Day,
+            undefined,
+            '[]'
+        );
     const isFinishBetween1stLimitAnd2ndLimit = finishMoment.isBetween(
         nightMomentLimit,
         daytimeMomentLimit,
@@ -130,18 +221,47 @@ const determineNumbers = (startMoment, finishMoment) => {
         '[]'
     );
 
-    const isStartBetween2ndLimitAnd24 = startMoment.isBetween(
-        daytimeMomentLimit,
-        twentyFourHoursMoment,
-        undefined,
-        '[]'
-    );
+    const isStartBetween2ndLimitAnd24 =
+        startMoment.isBetween(
+            daytimeMomentLimit,
+            twentyFourHoursMoment,
+            undefined,
+            '[]'
+        ) ||
+        startMoment.isBetween(
+            daytimeMomentLimitMinus1Day,
+            twentyFourHoursMomentMinus1Day,
+            undefined,
+            '[]'
+        );
     const isFinishBetween2ndLimitAnd24 = finishMoment.isBetween(
         daytimeMomentLimit,
         twentyFourHoursMoment,
         undefined,
         '[]'
     );
+
+    return {
+        isStartBetween0And1stLimit,
+        isFinishBetween0And1stLimit,
+        isStartBetween1stLimitAnd2ndLimit,
+        isFinishBetween1stLimitAnd2ndLimit,
+        isStartBetween2ndLimitAnd24,
+        isFinishBetween2ndLimitAnd24,
+    };
+};
+
+// Eu poderia ter usado algum design patter nessa função para deixar
+// ela com menos ifs, mas estou com pouco tempo.
+const determineNumbers = (startMoment, finishMoment) => {
+    const {
+        isStartBetween0And1stLimit,
+        isFinishBetween0And1stLimit,
+        isStartBetween1stLimitAnd2ndLimit,
+        isFinishBetween1stLimitAnd2ndLimit,
+        isStartBetween2ndLimitAnd24,
+        isFinishBetween2ndLimitAnd24,
+    } = isBetweenHours(startMoment, finishMoment);
 
     if (isStartBetween0And1stLimit) {
         if (isFinishBetween0And1stLimit) {
@@ -181,7 +301,6 @@ const calculateHoursPerPeriod = (startTime, finishTime) => {
 
     // Caculating the daytime hours and night hours
     const functionName = letter1 + number1 + letter2 + number2;
-    console.log('functionName', functionName);
     const {daytimeHours, nightHours} = calculate[functionName](
         startMoment,
         finishMoment
